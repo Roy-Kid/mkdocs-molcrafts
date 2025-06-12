213a5434a6b959c6e5fa9f932752edc2f60addf3
Jichen Li <42854324+Roy-Kid@users.noreply.github.com>
2025-06-12 16:22:36 +0200

    Refactor layouts and theme provider

--- components/theme-config.tsx
+++ components/theme-config.tsx
@@ -1,7 +1,8 @@
-const raw = document.getElementById('mkdocs-config').textContent;
+const raw = document.getElementById("mkdocs-config").textContent;
 const themeConfig = JSON.parse(raw);
 interface IThemeConfig {
-    site_name: string;
+  site_name: string;
+  nav?: Array<Record<string, string>>;
 }
 export { themeConfig };
 export type { IThemeConfig };
