6362b50d80998fb699b063faab2190db4b632f56
Jichen Li <42854324+Roy-Kid@users.noreply.github.com>
2025-06-12 22:12:33 +0200

    Add page data extraction and integrate sidebar toc

--- src/main.tsx
+++ src/main.tsx
@@ -1,35 +1,7 @@
-import { DocumentBody } from "@/components/document-body";
-import { BaseLayout, DocumentationLayout } from "@/components/layout";
-import { DocSidebar } from "@/components/sidebar";
-import { themeConfig } from "@/components/theme-config";
-import { MolCraftsDocThemeProvider } from "@/components/theme-provider";
-import * as React from "react";
-import { createRoot } from "react-dom/client";
-
-const extractPageContent = () => {
-    const page_content_container = document.getElementById("page-content");
-    if (page_content_container) {
-        const page_content = page_content_container.innerHTML;
-        page_content_container.remove();
-        return page_content;
-    }
-}
-
-const extractPageTOC = () => {
-    const page_toc_container = document.getElementById("page-toc");
-    if (page_toc_container) {
-        const page_toc = page_toc_container.innerHTML;
-        page_toc_container.remove();
-        return page_toc;
-    }
-}
-
-export const MainPage = () => {
-
-    const pageContent = extractPageContent();
-    const pageTOC = extractPageTOC();
-
-  return (
+import { pageContent, toc } from "@/components/page-data";
+const sections = toc.map((item) => ({ id: item.id, title: item.title }));
+          {/* biome-ignore lint/security/noDangerouslySetInnerHtml: page content comes from mkdocs */}
+          <div dangerouslySetInnerHTML={{ __html: pageContent }} />
     <MolCraftsDocThemeProvider>
       <BaseLayout config={themeConfig}>
         <DocumentationLayout>
