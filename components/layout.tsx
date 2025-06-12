394a773f9150a359b4b317bd9d636963becf941f
Roy Kid <lijichen365@gmail.com>
2025-06-12 14:28:38 +0000

    add side bar

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,4 +1,5 @@
 import type { ReactNode } from "react";
+import type { IThemeConfig } from "./theme-config";
 
 interface DocumentationLayoutProps {
   children?: ReactNode;
@@ -15,5 +16,20 @@ export interface NotFoundLayoutProps {
 export const NotFoundLayout = ({ children }: NotFoundLayoutProps) => {
   return (
     <div className="flex items-center justify-center py-20">{children}</div>
+  );
+};
+
+interface BaseLayoutProps {
+  children: ReactNode;
+  config: IThemeConfig;
+}
+
+export const BaseLayout = ({ children, config }: BaseLayoutProps) => {
+  return (
+    <div className="min-h-screen flex flex-col bg-background">
+      <DocsNavbar site_name={config.site_name} nav={config.nav} />
+      <main className="flex-1 container mx-auto px-4 pt-4">{children}</main>
+      <DocsFooter site_name={config.site_name} />
+    </div>
   );
 };
